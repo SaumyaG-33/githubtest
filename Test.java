25a55ab3dccdd99fc2f2dff6c6df40eee126b03c
saumya <saumyag335@gmail.com>
2023-09-09 14:33:24 -0400

    functions A B

--- Test.java
+++ Test.java
@@ -5,4 +5,13 @@ import java.sql.SQLOutput;
 public class Test {
     System.out.println("hello my name is Saumya Gupta");
     System.out.println("Hi im Harshit, im your partner for lab 2, we'll work on this together");
+    public void functionA(){
+
+    }
+
+    public void functionB(){
+        
+    }
+
+
 }
