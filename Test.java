b55c67e7307dc6eb601009445ae57bba1358cc44
Harshit Gupta <hersheyg0105@gmail.com>
2023-09-09 13:24:44 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,4 +4,5 @@ import java.sql.SQLOutput;
 
 public class Test {
     System.out.println("hello my name is Saumya Gupta");
+    System.out.println("Hi im Harshit, im your partner for lab 2, we'll work on this together");
 }
